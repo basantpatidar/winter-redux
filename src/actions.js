e1fe9a16b61407a671a917f8405fcdfced09241c
Basant <basant.send@gmail.com>
2019-11-25 19:46:16 +0000

    to pull in pc

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,20 @@
 import axios from "axios";
+import { async } from "q";
 const CURRENT_WEATHER = 'CURRENT_WEATHER';
+const SET_WEATHER = "SET_WEATHER";
 
-export const currentWeather = (city) => {
-  const res = axios.get("");
-  return {
+export const currentWeather = city => async dispatch => {
+  const res = await axios.get(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=31jfdjgdghdfhbjjfhgfga32a`);
+  console.log(res);
+  
+  dispatch ({
     type: CURRENT_WEATHER,
     payload: city
-  }
+  });
+
+  // const res = await axios.get(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=31e12e179d9d9f7a0e0ba5d7c45a32a6`);
+  // dispatch({
+  //   type: "SET_WEATHER",
+  //   payload: res
+  // })
 }
