7c077fcf8cd2487b7a4e27e8140ba3af439abac7
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-11-28 14:55:38 -0500

    git fix

--- src/reducers.js
+++ src/reducers.js
@@ -5,13 +5,11 @@ const initialState = {
   apiResponse: null
 <<<<<<< HEAD
 };
-=======
-}
->>>>>>> 92c1709e1959979930d627d7c56967187277f3dd
+
 
 
 const searchReducer = (state = initialState, action) => {
-<<<<<<< HEAD
+
   if (action.type === 'CITY') {
     console.log(state, ' -< - <---  City  action');
     return {
@@ -33,28 +31,6 @@ const searchReducer = (state = initialState, action) => {
   //  }
   return state;
 };
-=======
- if( action.type === "CITY" ){
-   
-     console.log(state ," -< - <---  City  action");
-   return {
-     ...state, city : action.payload
-   }
- }
- if(action.type === "ZIP_CODE"){
-   console.log("ZipCode");
-   return {
-     ...state, zipCode: action.payload
-   }
- }
-//  if(action.type === "SET_WEATHER"){
-//    return {
-//      ...state, apiResponse : action.payload
-//    }
-//  } 
- return state;
-}
->>>>>>> 92c1709e1959979930d627d7c56967187277f3dd
 export default combineReducers({
   searchReducer
 });
