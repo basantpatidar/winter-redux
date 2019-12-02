cdde9af546014a0b7c858ad8d2539e55fa326ddd
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-12-01 22:05:21 -0500

    git incoming change accepted

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as action from './actions';
-<<<<<<< HEAD
 import SearchResults from './SearchResults';
 import axios from 'axios';
 
@@ -17,7 +16,7 @@ class SearchBar extends React.Component {
   callApiHere = () => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${this.props.searchInput.city}&appid=31e12e179d9d9f7a0e0ba5d7c45a32a6`
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.props.searchInput.city}&appid=`
       )
       .then(res => this.props.SearchResult(res.data));
   };
@@ -32,7 +31,7 @@ class SearchBar extends React.Component {
           onChange={this.handleChangeCity}
         />{' '}
         <br></br>
-        <input type="text" name="zipcode" onChange={this.handleChangeZip} />
+        <input type="text" name="country" onChange={this.handleChangeZip} />
         <button onClick={this.callApiHere}> Submit </button>
         <SearchResults />
       </div>
@@ -45,46 +44,3 @@ const mapStateToProps = state => {
   };
 };
 export default connect(mapStateToProps, action)(SearchBar);
-
-//can I send data from both the input tags in single handleChange?
-=======
-import SearchResults from "./SearchResults"
-import axios from 'axios';
-
-class SearchBar extends React.Component{
-  handleChangeCity = (event) => {
-    this.props.city( event.target.value);
-    console.log(this.props.apiResponse, "This is handleChange City ");
-  }
-  handleChangeZip = (event) => {
-    this.props.zip(event.target.value);
-    console.log(this.props.apiResponse, "This is handleChange Zip");
-  }
-  callApiHere = () => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.searchInput.city}&appid=`)
-    .then(res => 
-    this.props.SearchResult(res.data)
-    )
-  }
-
-render(){
-  
-   return(
-    <div>
-    <h3> Enter City name </h3>
-    <input type = "text" name = 'search' onChange={this.handleChangeCity}/> <br></br>
-    <input type = "text" name = 'country' onChange={this.handleChangeZip}/>
-    <button onClick={this.callApiHere}> Submit </button>
-
-    <SearchResults/>
-    </div> 
-  )
-}
-}
-const mapStateToProps = (state) => {
-    return{
-      searchInput: state.searchReducer
-    }
-}
-export default connect(mapStateToProps, action)(SearchBar);
->>>>>>> 92c1709e1959979930d627d7c56967187277f3dd
