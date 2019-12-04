94c7c9b7b501df3c9333d4c1f6d9beab7eae66ae
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-12-03 21:47:02 -0500

    debugging

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,36 +1,36 @@
-import React from "react";
-import {connect} from 'react-redux';
-const SearchResult = (props) => {
-//It the view 
-//everything after search result should be dispalyed here
-const renderThis = () => {
-    const { apiResponse }
- = props.searchReducer;
-     return ( <div>
-       <p> {
-       apiResponse && apiResponse.main && apiResponse.main.temp       
-    } </p>
-    <p> {
-       apiResponse && apiResponse.weather && apiResponse.weather[0].main
-    } </p>
-    <p> {
-       apiResponse && apiResponse.sys && apiResponse.sys.country       
-    } </p>
+import React from 'react';
+import { connect } from 'react-redux';
+const SearchResult = props => {
+  //It the view
+  //everything after search result should be dispalyed here
+  const renderThis = () => {
+    const { apiResponse } = props.searchReducer;
+    return (
+      <div>
+        <p> {apiResponse && apiResponse.main && apiResponse.main.temp} </p>
+        <p>
+          {' '}
+          {apiResponse &&
+            apiResponse.weather &&
+            apiResponse.weather[0].main}{' '}
+        </p>
+        <p> {apiResponse && apiResponse.sys && apiResponse.sys.country} </p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <p>This is Search Results file</p>
+      {renderThis()}
     </div>
-    )
-  }
+  );
+};
 
-return(
-  <div>
-     { renderThis()}
-    </div> 
-);
-}
-
-const mapStateToProps = (state) => {
-  console.log(state)
+const mapStateToProps = state => {
+  console.log(state);
   return {
     searchReducer: state.searchReducer
-  }
-}
-export default  connect(mapStateToProps)(SearchResult);
+  };
+};
+export default connect(mapStateToProps)(SearchResult);
