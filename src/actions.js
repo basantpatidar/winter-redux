3e27ffa21c7e3582083970eeff2823f7a882c743
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-12-06 22:46:10 -0500

    update

--- src/actions.js
+++ src/actions.js
@@ -1,39 +1,35 @@
 // import axios from "axios";
 const CITY = 'CITY';
-const SET_WEATHER = "SET_WEATHER";
-const ZIP_CODE = 'ZIP_CODE'
+const SET_WEATHER = 'SET_WEATHER';
+const ZIP_CODE = 'ZIP_CODE';
 
 export const city = (name, value) => async dispatch => {
-   
-  dispatch ({
+  dispatch({
     type: CITY,
-    payload: {[name]: value};
+    payload: { [name]: value }
   });
+};
 
-  
-}
-
-export const zip = (zip) => async dispatch => {
+export const zip = zip => async dispatch => {
   dispatch({
     type: ZIP_CODE,
     payload: zip
-  })
-}
-export const hour = (hour) => async dispatch => {
+  });
+};
+export const hour = hour => async dispatch => {
   dispatch({
     type: HOURLY,
     payload: hour
-  })
-}
+  });
+};
 
-export const SearchResult = (res) => dispatch => {
+export const SearchResult = res => dispatch => {
   // const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`);
   dispatch({
     type: SET_WEATHER,
     payload: res
   });
-}
+};
 
 //How do reducers know that specifically only that reduce is got called?
 //I mean that there are several reducers so do every action gets checked in each reducer?
-
